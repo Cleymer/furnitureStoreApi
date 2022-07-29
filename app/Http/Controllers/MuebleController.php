bf3e870ab5da73145472c54e55cd6f7882de29d9
cleymer <cleymer.zavala8807@est.uca.edu.ni>
2022-07-29 16:05:50 -0600

    Mueble CRUD

--- app/Http/Controllers/MuebleController.php
+++ app/Http/Controllers/MuebleController.php
@@ -4,7 +4,102 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Resources\MuebleResource;
+use App\Models\Mueble;
+use Validator;
+
 class MuebleController extends Controller
 {
     //
+    public function index() {
+        return MuebleResource::collection(Mueble::all());
+    }
+
+    public function show($id) {
+        
+        $mueble = Mueble::find($id);
+
+        if(!$mueble){
+            return [
+                'message' => 'Mueble does not existe',
+            ];
+        }
+
+        return response()->json([
+            'success' => 'true',
+            'data' => new MuebleResource($mueble)
+        ]);
+
+    }
+
+    public function store(Request $request) {
+
+        $validateData = Validator::make($request->all(),
+            [
+                'name' => 'required',
+                'description' => 'required',
+                'category_id' => 'required'
+            ]
+        );
+
+        if($validateData->fails()){
+            return [
+                'success' => 'Can not create the mueble',
+                'error' => $validateData->errors()
+            ];
+        }
+
+        $mueble = Mueble::create($request->all());
+
+        return response()->json([
+            'success' => 'Mueble created',
+            'data' => new MuebleResource($mueble)
+        ]);
+
+    }
+
+
+    public function update(Request $request, Mueble $mueble) {
+
+        return [
+            'data' => $request->all(),
+            'muebel' => $mueble->all()
+        ];
+            
+            
+
+        $validateData = Validator::make($request->all(),
+            [
+                'name' => 'required',
+                'description' => 'required',
+                'category_id' => 'required'
+            ]
+        );
+
+        if($validateData->fails()){
+            return [
+                'success' => 'Can not update the mueble',
+                'error' => $validateData->errors()
+            ];
+        }
+        
+        $mueble->update($request->all());
+
+        return response()->json([
+            'success' => 'true',
+            'data' => new MuebleResource($mueble)
+        ]);
+
+    }
+
+
+    public function destroy(Mueble $mueble){
+
+        $mueble->delete();
+
+        return response()->json([
+            'success' => "Mueble deleted"
+        ]);
+
+    }
 }
